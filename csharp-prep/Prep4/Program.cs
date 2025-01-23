690630f972c0a1efeb7ec9423a0e34d447ecc09a
Sambradshaw19011 <168466374+Sambradshaw19011@users.noreply.github.com>
2025-01-23 10:24:44 -0700

    Learning Activity - C# Prep 4 & 5 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,15 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers;
-        numbers = new List<int>();
+        List<int> numbers = new List<int>();
+        int new_number;
 
-        while (new_number != 0)
+        while (true)
         {
-            Console.Write("What is your guess? ");
+            Console.Write("Add a new number (type 0 to stop): ");
             new_number = int.Parse(Console.ReadLine());
+
+            if (new_number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(new_number);
+
+            Console.WriteLine("\nCurrent list:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        Console.WriteLine("\nFinal list:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
         }
     }
+}
