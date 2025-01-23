ed4fbac2c0f7e5a2618da54b987a9a828503ac90
Sambradshaw19011 <168466374+Sambradshaw19011@users.noreply.github.com>
2025-01-23 09:58:14 -0700

    Learning Activity - C# Prep 3 Complete Prep 4 updates

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 11);
+
+        int guess = 0;
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+
+        while (new_number != 0)
+        {
+            Console.Write("What is your guess? ");
+            new_number = int.Parse(Console.ReadLine());
+        }
     }
-}
